91f8fff09990554184b99d6b96ae25a4674e3de3
Vinícius Gomes <neogedom@gmail.comm>
2020-09-24 20:15:23 -0400

    adding toRIPE MD 160 method

--- src/main/java/com/blockchain/CriptoUtils.java
+++ src/main/java/com/blockchain/CriptoUtils.java
@@ -1,30 +1,18 @@
 package com.blockchain;
 
 import java.io.UnsupportedEncodingException;
-import java.security.GeneralSecurityException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.ECPublicKey;
-import java.security.spec.ECGenParameterSpec;
-import java.security.spec.ECPoint;
+import java.security.NoSuchProviderException;
 
 public class CriptoUtils {
+    private static MessageDigest digest = null;
+    private static byte[] bytes = null;
+    private static StringBuffer buffer = null;
 
-    private static PublicKey publicKey;
-    // private static PrivateKey privateKey;
-
-    public static String toHash256(Object obj) {
-        MessageDigest digest = null;
-        byte[] bytes = null;
-        StringBuffer buffer = null;
+    public static String toSHA256(Object obj) {
 
         try {
-
             digest = MessageDigest.getInstance("SHA-256");
             bytes = digest.digest(obj.toString().getBytes("UTF-8"));
 
@@ -41,49 +29,16 @@ public class CriptoUtils {
         return buffer.toString();
     }
 
-    public static String getPublicKey() {
-
-        getPrivateKey();
-        ECPublicKey ecPublicKey = (ECPublicKey) publicKey;
-        ECPoint point = ecPublicKey.getW();
-        String sx = adjustTo64(point.getAffineX().toString(16)).toUpperCase();
-        String sy = adjustTo64(point.getAffineY().toString(16)).toUpperCase();
-        String bcPub = "04" + sx + sy;
-        
-        return "bcPub: " + bcPub;
-    }
-
-    public static String getPrivateKey() {
-
-        String strPrivateKey = "";
+    public static String toRIPEMD160(Object obj) {
         try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
-            ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
-            keyPairGenerator.initialize(ecSpec);
-            KeyPair pair = keyPairGenerator.genKeyPair();
-            PrivateKey privateKey = pair.getPrivate();
-            publicKey = pair.getPublic();
-
-            ECPrivateKey ecPrivateKey = (ECPrivateKey) privateKey;
-            strPrivateKey =  adjustTo64(ecPrivateKey.getS().toString(16).toUpperCase());
-
-        } catch (GeneralSecurityException e) {
+            digest = MessageDigest.getInstance("RipeMD160", "BC");
+            bytes = digest.digest(obj.toString().getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | UnsupportedEncodingException e) {
             
             e.printStackTrace();
         }
-
-        return "s[" + strPrivateKey.length() +"]: " +  strPrivateKey;
+        
+        return buffer.toString();
     }
 
-
-
-    private static String adjustTo64(String s) {
-        switch (s.length()) {
-            case 62: return "00" + s;
-            case 63: return "0" + s;
-            case 64: return s;
-            default:
-                throw new IllegalArgumentException("not a valid key: " + s);
-        }
-    }
 }
--- src/main/java/com/blockchain/Wallet.java
+++ src/main/java/com/blockchain/Wallet.java
@@ -1,8 +1,67 @@
 package com.blockchain;
 
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.ECPoint;
+
 public class Wallet {
-    public static String generateAddress(String privateKey, String message) {
-        String signature = "";
-        return signature;
+    private String address;
+    private  PublicKey publicKey;
+    private  PrivateKey privateKey;
+
+    public String getPublicKey() {
+
+        getPrivateKey();
+        ECPublicKey ecPublicKey = (ECPublicKey) publicKey;
+        ECPoint point = ecPublicKey.getW();
+        String sx = adjustTo64(point.getAffineX().toString(16)).toUpperCase();
+        String sy = adjustTo64(point.getAffineY().toString(16)).toUpperCase();
+        String bcPub = "04" + sx + sy;
+        
+        return bcPub;
+    }
+
+    public String getPrivateKey() {
+
+        String strPrivateKey = "";
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
+            keyPairGenerator.initialize(ecSpec);
+            KeyPair pair = keyPairGenerator.genKeyPair();
+            privateKey = pair.getPrivate();
+            publicKey = pair.getPublic();
+
+            ECPrivateKey ecPrivateKey = (ECPrivateKey) privateKey;
+            strPrivateKey =  adjustTo64(ecPrivateKey.getS().toString(16).toUpperCase());
+
+        } catch (GeneralSecurityException e) {
+            
+            e.printStackTrace();
+        }
+        return "s[" + strPrivateKey.length() +"]: " +  strPrivateKey;
+    }
+
+
+
+    private String adjustTo64(String s) {
+        switch (s.length()) {
+            case 62: return "00" + s;
+            case 63: return "0" + s;
+            case 64: return s;
+            default:
+                throw new IllegalArgumentException("not a valid key: " + s);
+        }
+    }
+    
+    public String generateAddress(String message) {
+        CriptoUtils.toSHA256(getPublicKey());
+        return address;
     }
 }
